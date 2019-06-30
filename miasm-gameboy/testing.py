7daa470a6446d195651d046a035bf0b206ebf9cf
Calle Svensson <calle.svensson@zeta-two.com>
2019-06-30 03:49:15 +0200

    miasm gameboy module structure

--- miasm-gameboy/testing.py
+++ miasm-gameboy/testing.py
@@ -6,18 +6,19 @@ logging.basicConfig(level=logging.DEBUG)
 from miasm.analysis.machine import Machine
 from miasm.analysis import binary
 
-import container_gameboy
+import gameboy
 
 #ROM_PATH = 'test/tetris.gb'
 ROM_PATH = 'test/gb-calc.gb'
 
-with open(ROM_PATH, 'rb') as fin:
-    bi = binary.Container.from_stream(fin, ignore_checksum=True)
-    print('bin_stream: %s' % bi.bin_stream)
-    print('executable: %s' % bi.executable)
-    print('entry_point: %s' % bi.entry_point)
-    print('arch: %s' % bi.arch)
-    print('loc_db: %s' % bi.loc_db)
+if __name__ == '__main__':
+    with open(ROM_PATH, 'rb') as fin:
+        bi = binary.Container.from_stream(fin, ignore_checksum=True)
+        print('bin_stream: %s' % bi.bin_stream)
+        print('executable: %s' % bi.executable)
+        print('entry_point: %s' % bi.entry_point)
+        print('arch: %s' % bi.arch)
+        print('loc_db: %s' % bi.loc_db)
 
-#machine = Machine('gameboy')
-#mn, dis_engine_cls, ira_cls = machine.mn, machine.dis_engine, machine.ira
+    #machine = Machine('gameboy')
+    #mn, dis_engine_cls, ira_cls = machine.mn, machine.dis_engine, machine.ira
